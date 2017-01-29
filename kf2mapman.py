66dc09c5031644f4e3fad3970642b38b80eaa198
Andrew Eaton <null@dys4ik.com>
2017-01-28 20:12:13 -0500

    Added map rotation string

--- kf2mapman.py
+++ kf2mapman.py
@@ -1,7 +1,13 @@
+from os import listdir
+from os.path import isfile, isdir, join
+import re
 import sys
 import textwrap
 
 DEFAULT_SCREENSHOT='UI_MapPreview_TEX.UI_MapPreview_Placeholder'
+FILE_PATTERN = re.compile('.*.kfm$', re.IGNORECASE)
+NAME_PATTERN = re.compile('^([^.]*).kfm$', re.IGNORECASE)
+USAGE = 'Usage:\nkf2mapman.py <mapdir>'
 
 # [KF-Default KFMapSummary]
 # MapName=KF-Default
@@ -15,13 +21,38 @@ def data_store(name, screenshot=DEFAULT_SCREENSHOT):
     ScreenshotPathName={1}
     """.format(name, screenshot))
 
-def map_list(names):
+def get_map_names(directory):
+    '''Return a list of map names for a given directory'''
+    try:
+        return maplist
+    except UnboundLocalError:
+        maplist = [
+            re.search(NAME_PATTERN, name).group(1)
+            for name in listdir(directory)
+            if isfile(join(directory, name))
+            and re.match(FILE_PATTERN, name)
+        ]
+        return maplist
+
+def create_map_list(names):
     '''Returns a list of data stores for the given map names'''
     return ''.join(
         [data_store(name) for name in names]
     )
 
-if __name__ == __name__:
-    with open(sys.argv[1], 'r') as f:
-        maplist = [m for m in f.read().split()]
-    print map_list(maplist)
+def create_map_rotation(names):
+    return ','.join([
+        '"%s"' % name
+        for name in names
+    ])
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and isdir(sys.argv[1]):
+        directory = sys.argv[1]
+        print '\nrotation string:'
+        print create_map_rotation(get_map_names(directory))
+        print '\nmap entries:'
+        print create_map_list(get_map_names(directory))
+    else:
+        print USAGE
+        sys.exit()
--- test_kf2mapman.py
+++ test_kf2mapman.py
@@ -1,4 +1,4 @@
-import sys
+import os
 import textwrap
 import kf2mapman
 
@@ -9,9 +9,10 @@ def test_data_store():
         ScreenshotPathName=UI_MapPreview_TEX.UI_MapPreview_Placeholder
     """)
     results = kf2mapman.data_store('KF-Default')
+    print results
     assert results == expected
 
-def test_map_list():
+def test_create_map_list():
     expected = textwrap.dedent("""
         [KF-Default KFMapSummary]
         MapName=KF-Default
@@ -21,5 +22,23 @@ def test_map_list():
         MapName=KF-Default2
         ScreenshotPathName=UI_MapPreview_TEX.UI_MapPreview_Placeholder
     """)
-    results = kf2mapman.map_list(['KF-Default', 'KF-Default2'])
+    results = kf2mapman.create_map_list(['KF-Default', 'KF-Default2'])
+    print results
     assert results == expected
+
+def test_get_map_names():
+    testmaps_dir = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'testmaps'
+    )
+    expected = ['KF-Default', 'KF-Default2']
+    results = kf2mapman.get_map_names(testmaps_dir)
+    print results
+    assert expected == results
+
+def test_create_map_rotation():
+    names = ['KF-Default', 'KF-Default2']
+    expected = '"KF-Default","KF-Default2"'
+    results = kf2mapman.create_map_rotation(names)
+    print results
+    assert expected == results
