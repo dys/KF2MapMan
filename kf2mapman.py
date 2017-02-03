960c386dbeaa2255e35053caf0522513a3ded470
Andrew Eaton <null@dys4ik.com>
2017-02-03 13:33:03 -0500

    Cleanup and new tests

--- kf2mapman.py
+++ kf2mapman.py
@@ -1,58 +1,76 @@
 from os import listdir
 from os.path import isfile, isdir, join
+import ConfigParser
 import re
 import sys
 import textwrap
 
-DEFAULT_SCREENSHOT='UI_MapPreview_TEX.UI_MapPreview_Placeholder'
-FILE_PATTERN = re.compile('.*.kfm$', re.IGNORECASE)
-NAME_PATTERN = re.compile('^([^.]*).kfm$', re.IGNORECASE)
 USAGE = 'Usage:\nkf2mapman.py <mapdir>'
+DEFAULT_SCREENSHOT='UI_MapPreview_TEX.UI_MapPreview_Placeholder'
+FILE_PATTERN = '.*.kfm$'
+NAME_PATTERN = '^([^.]*).kfm$'
 
 # [KF-Default KFMapSummary]
 # MapName=KF-Default
 # ScreenshotPathName=UI_MapPreview_TEX.UI_MapPreview_Placeholder
-def data_store(name, screenshot=DEFAULT_SCREENSHOT):
-    '''Returns a map data store for the given map name. Optionally provide a screenshot.'''
+def create_data_store(name):
+    '''Returns a map data store entry for the given map name.'''
     return textwrap.dedent(
     """
     [{0} KFMapSummary]
     MapName={0}
     ScreenshotPathName={1}
-    """.format(name, screenshot))
-
-def get_map_names(directory):
-    '''Return a list of map names for a given directory'''
-    try:
-        return maplist
-    except UnboundLocalError:
-        maplist = [
-            re.search(NAME_PATTERN, name).group(1)
-            for name in listdir(directory)
-            if isfile(join(directory, name))
-            and re.match(FILE_PATTERN, name)
-        ]
-        return maplist
+    """.format(name, DEFAULT_SCREENSHOT))
 
 def create_map_list(names):
     '''Returns a list of data stores for the given map names'''
     return ''.join(
-        [data_store(name) for name in names]
+        [create_data_store(name) for name in names]
     )
 
 def create_map_rotation(names):
+    '''Create the map rotation string for MapCycle'''
     return ','.join([
         '"%s"' % name
         for name in names
     ])
 
+
+def get_maps_in_dir(directory):
+    '''Return a list of map names (minus extension) for a given directory.'''
+    return [
+        re.search(re.compile(
+            NAME_PATTERN, re.IGNORECASE), name) .group(1)
+        for name in listdir(directory)
+        if isfile(join(directory, name))
+        and re.match(re.compile(
+            FILE_PATTERN, re.IGNORECASE), name)
+    ]
+
+
+def read_ini(ini):
+    '''Returns the given ini file as a ConfigParser object'''
+    with open(ini, 'r') as configfile:
+        config = ConfigParser.ConfigParser()
+        config.read(configfile)
+        return config
+
+def get_current_maps(config):
+    '''Returns a list of maps from the given ConfigParser object'''
+    return [
+        config.get(name, 'MapName')
+        for name in config.sections()
+        if 'KFMapSummary' in name
+    ]
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 2 and isdir(sys.argv[1]):
         directory = sys.argv[1]
         print '\nrotation string:'
-        print create_map_rotation(get_map_names(directory))
+        print create_map_rotation(get_maps_in_dir(directory))
         print '\nmap entries:'
-        print create_map_list(get_map_names(directory))
+        print create_map_list(get_maps_in_dir(directory))
     else:
         print USAGE
         sys.exit()
--- test_kf2mapman.py
+++ test_kf2mapman.py
@@ -1,44 +1,67 @@
-import os
-import textwrap
 import kf2mapman
 
-def test_data_store():
+import ConfigParser
+import os
+import shutil
+import textwrap
+
+TEST_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'testmaps')
+PRE_EDIT_INI = os.path.join(TEST_DIR, 'pre-edit.ini')
+EDITED_INI = os.path.join(TEST_DIR, 'to-edit.ini')
+POST_EDIT_INI = os.path.join(TEST_DIR, 'post-edit.ini')
+MAP_NAMES = ['KF-Map1', 'KF-Map2']
+NEW_MAP_NAMES = ['KF-Newmap1', 'KF-Newmap2']
+
+def test_create_data_store():
     expected = textwrap.dedent("""
-        [KF-Default KFMapSummary]
-        MapName=KF-Default
+        [KF-Newmap1 KFMapSummary]
+        MapName=KF-Newmap1
         ScreenshotPathName=UI_MapPreview_TEX.UI_MapPreview_Placeholder
     """)
-    results = kf2mapman.data_store('KF-Default')
-    print results
+    results = kf2mapman.create_data_store('KF-Newmap1')
     assert results == expected
 
 def test_create_map_list():
     expected = textwrap.dedent("""
-        [KF-Default KFMapSummary]
-        MapName=KF-Default
+        [KF-Newmap1 KFMapSummary]
+        MapName=KF-Newmap1
         ScreenshotPathName=UI_MapPreview_TEX.UI_MapPreview_Placeholder
 
-        [KF-Default2 KFMapSummary]
-        MapName=KF-Default2
+        [KF-Newmap2 KFMapSummary]
+        MapName=KF-Newmap2
         ScreenshotPathName=UI_MapPreview_TEX.UI_MapPreview_Placeholder
     """)
-    results = kf2mapman.create_map_list(['KF-Default', 'KF-Default2'])
-    print results
+    results = kf2mapman.create_map_list(NEW_MAP_NAMES)
     assert results == expected
 
-def test_get_map_names():
-    testmaps_dir = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        'testmaps'
-    )
-    expected = ['KF-Default', 'KF-Default2']
-    results = kf2mapman.get_map_names(testmaps_dir)
-    print results
-    assert expected == results
-
 def test_create_map_rotation():
-    names = ['KF-Default', 'KF-Default2']
-    expected = '"KF-Default","KF-Default2"'
-    results = kf2mapman.create_map_rotation(names)
-    print results
-    assert expected == results
+    expected = '"KF-Newmap1","KF-Newmap2"'
+    results = kf2mapman.create_map_rotation(NEW_MAP_NAMES)
+    assert results == expected
+
+
+def test_get_maps_in_dir():
+    results = kf2mapman.get_maps_in_dir(TEST_DIR)
+    assert results == NEW_MAP_NAMES
+
+def test_read_ini():
+    #config = ConfigParser.ConfigParser()
+    results = kf2mapman.read_ini(EDITED_INI)
+    assert isinstance(results, ConfigParser.ConfigParser)
+
+def test_get_maps_from_config():
+    config = ConfigParser.ConfigParser()
+    config.read(PRE_EDIT_INI)
+    results = kf2mapman.get_current_maps(config)
+    assert results == MAP_NAMES
+
+
+#def test_edit_ini_file():
+#    shutil.copy(PRE_EDIT_INI, EDITED_INI)
+#    kf2mapman.add_maps_to_ini(NEW_MAP_NAMES, EDITED_INI)
+#    expected = open(POST_EDIT_INI, 'r').read()
+#    results = open(EDITED_INI, 'r').read()
+#    assert results == expected
+#    os.remove(EDITED_INI)
